c18e56a8d0156954c4f06f28e462c0e1a482bbeb
nmatison <nmatison@gmail.com>
2018-09-19 16:18:44 -0700

    Finish phase 1, 2, 3 of Rails Lite

---
+++ rails_lite/lib/controller_base.rb
@@ -0,0 +1,62 @@
+require 'active_support'
+require 'active_support/core_ext'
+# require 'active_support/inflector'
+require 'erb'
+require_relative './session'
+require 'byebug'
+
+class ControllerBase
+  attr_reader :req, :res, :params
+  attr_accessor :already_built_response
+
+  # Setup the controller
+  def initialize(req, res)
+    @req = req
+    @res = res
+    @already_built_response = false
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+    @already_built_response
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+    raise 'you goofed' if @already_built_response
+    @res.status = 302
+    @res['Location'] = url
+    @already_built_response = true
+    nil
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+    raise 'you goofed' if @already_built_response
+    @res.write(content)
+    @res['content-type'] = content_type
+    @already_built_response = true
+    nil
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+    dir_path = File.dirname(__FILE__)
+    controller = self.class.name.underscore
+    # dir_path = "/Users/appacademy/Desktop/w5d3/rails_lite/"
+    template_path = File.join(dir_path, "..", "views", controller, "#{template_name}.html.erb")
+    template_code = File.read(template_path)
+    render_content(ERB.new(template_code).result(binding), 'text/html')
+  end
+
+  # method exposing a `Session` object
+  def session
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+  end
+end
